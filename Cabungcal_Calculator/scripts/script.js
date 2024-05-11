7b4b8a7be2ddde583a7d52e429c48fb4a00121f0
Charyll <148406119+asdfghjk000@users.noreply.github.com>
2024-05-11 17:07:58 +0800

    Add files via upload

---
+++ Cabungcal_Calculator/scripts/script.js
@@ -0,0 +1,86 @@
+const lightTheme = "styles/light.css";
+const darkTheme = "styles/dark.css";
+const sunIcon = "assets/SunIcon.svg";
+const moonIcon = "assets/MoonIcon.svg";
+const themeIcon = document.getElementById("theme-icon");
+const res = document.getElementById("result");
+const toast = document.getElementById("toast");
+
+let isNewCalculation = true; // Flag to track if a new calculation is started
+
+function calculate(value) {
+  const calculatedValue = eval(value || null);
+  if (isNaN(calculatedValue)) {
+    res.value = "Can't divide 0 with 0";
+    setTimeout(() => {
+      res.value = "";
+    }, 1300);
+  } else {
+    res.value = calculatedValue;
+    isNewCalculation = true; // Reset flag after calculation
+  }
+}
+
+// Swaps the stylesheet to achieve dark mode.
+function changeTheme() {
+  const theme = document.getElementById("theme");
+  setTimeout(() => {
+    toast.innerHTML = "Calculator";
+  }, 1500);
+  if (theme.getAttribute("href") === lightTheme) {
+    theme.setAttribute("href", darkTheme);
+    themeIcon.setAttribute("src", sunIcon);
+    toast.innerHTML = "Dark Mode 🌙";
+  } else {
+    theme.setAttribute("href", lightTheme);
+    themeIcon.setAttribute("src", moonIcon);
+    toast.innerHTML = "Light Mode ☀️";
+  }
+}
+
+// Displays entered value on screen.
+function liveScreen(enteredValue) {
+  if (isNewCalculation) {
+    res.value = ""; // Clear the result if starting a new calculation
+    isNewCalculation = false; // Update flag
+  }
+  res.value += enteredValue;
+}
+
+//adding event handler on the document to handle keyboard inputs
+document.addEventListener("keydown", keyboardInputHandler);
+
+//function to handle keyboard inputs
+function keyboardInputHandler(e) {
+  // to fix the default behavior of browser,
+  // enter and backspace were causing undesired behavior when some key was already in focus.
+  e.preventDefault();
+  //grabbing the liveScreen
+
+  //numbers
+  if (e.key >= "0" && e.key <= "9") {
+    liveScreen(e.key);
+  }
+
+  //operators
+  if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/") {
+    liveScreen(e.key);
+  }
+
+  //decimal key
+  if (e.key === ".") {
+    liveScreen(".");
+  }
+
+  //press enter to see result
+  if (e.key === "Enter") {
+    calculate(res.value);
+  }
+
+  //backspace for removing the last input
+  if (e.key === "Backspace") {
+    const resultInput = res.value;
+    //remove the last element in the string
+    res.value = resultInput.substring(0, res.value.length - 1);
+  }
+}
